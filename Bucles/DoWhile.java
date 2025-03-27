9cefec2925037ec65c59c71c44a10a8f22f91676
soniamendoz <soniamendoza.14.com@gmail.com>
2025-03-27 22:13:48 +0100

    Add files via upload

---
+++ Bucles/DoWhile.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Bucles;
+
+/**
+ *< > [ ]
+ * @author LABO B
+ */
+public class DoWhile {
+  public static void main(String[] args) {
+        int contador = 1; // Inicializamos la variable
+
+        do {
+            System.out.println("Iteración número: " + contador);
+            contador++; // Incrementamos el contador
+        } while (contador <= 5); // Verificamos la condición después de ejecutar el código
+
+        System.out.println("Fin del bucle.");
+    }
+}
